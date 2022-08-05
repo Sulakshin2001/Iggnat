411836f88956aaaf26e6670c03d9e094520aca19
Sulakshin2001 <nikitasulaaskin@gmail.com>
2022-08-05 13:39:18 +0300

    Updates

---
+++ precache-manifest.849e9ce85c01f43bd913a39ee528f1df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82f9c88bbc2ae97cf4529664416044a3",
+    "url": "/Iggnat/index.html"
+  },
+  {
+    "revision": "c84960c98da75bf48bd3",
+    "url": "/Iggnat/static/css/main.d18971a0.chunk.css"
+  },
+  {
+    "revision": "31502918904a76de6d94",
+    "url": "/Iggnat/static/js/2.294abf1e.chunk.js"
+  },
+  {
+    "revision": "248fe9facfff898840212404b4cb88d9",
+    "url": "/Iggnat/static/js/2.294abf1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c84960c98da75bf48bd3",
+    "url": "/Iggnat/static/js/main.0cb03307.chunk.js"
+  },
+  {
+    "revision": "8428b218ecf7f851cc4a",
+    "url": "/Iggnat/static/js/runtime-main.33b94c75.js"
+  }
+]);
